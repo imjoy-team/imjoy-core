fb2e2fc6e49b57a86524da2439c4884fa4da8d1d
Wei Ouyang <oeway007@gmail.com>
2020-03-09 11:18:59 +0100

    use ChromeHeadless for karma

--- karma.conf.js
+++ karma.conf.js
@@ -75,7 +75,7 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['ChromeHeadlessNoSandbox', 'FirefoxHeadless'],
+        browsers: ['ChromeHeadless', 'FirefoxHeadless'],
 
         customLaunchers: {
             ChromeHeadlessNoSandbox: {
