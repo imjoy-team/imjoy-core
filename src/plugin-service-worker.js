f78483b7b86800cecb8b24e89b611d9f541e4adb
Wei Ouyang <oeway007@gmail.com>
2020-03-10 14:58:09 +0100

    send files to service-worker; improve code;

--- src/plugin-service-worker.js
+++ src/plugin-service-worker.js
@@ -30,9 +30,9 @@ if (typeof workbox !== "undefined") {
     new workbox.strategies.StaleWhileRevalidate()
   );
 
-  var plugin_requirements = new Set();
+  var cached_keys = new Set();
   function matchCb(request) {
-    return plugin_requirements.has(request.url.href);
+    return cached_keys.has(request.url.href);
   }
 
   workbox.routing.registerRoute(
@@ -40,105 +40,89 @@ if (typeof workbox !== "undefined") {
     new workbox.strategies.StaleWhileRevalidate()
   );
 
+  workbox.routing.setDefaultHandler(new workbox.strategies.NetworkOnly());
+
   caches.open(workbox.core.cacheNames.runtime).then(function(cache) {
     cache.keys().then(function(requests) {
       var urls = requests.map(function(request) {
         return request.url;
       });
-      plugin_requirements = new Set(urls);
-      console.log("cached requirements:", plugin_requirements);
+      cached_keys = new Set(urls);
+      console.log("cached requirements:", cached_keys);
     });
   });
-
-  var reg_match_url = /^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)([\/]{0,1}[^?#]*)(\?[^#]*|)(#.*|)$/;
-  function parseURL(href) {
-    var match = href.match(reg_match_url);
-    return (
-      (match && {
-        href: href,
-        protocol: match[1],
-        host: match[2],
-        hostname: match[3],
-        port: match[4],
-        pathname: match[5],
-        search: match[6],
-        hash: match[7],
-      }) ||
-      {}
-    );
-  }
 
   self.addEventListener("message", function(event) {
     if (event.data.action == "skipWaiting") self.skipWaiting();
     if (event.data && event.data.command) {
       // Use the Cache Storage API directly,
       // and add to the default runtime cache:
+      var resolve = function(result) {
+        event.ports[0].postMessage({ result: result });
+      };
+      var reject = function(error) {
+        event.ports[0].postMessage({ error: error });
+      };
+
       caches.open(workbox.core.cacheNames.runtime).then(function(cache) {
         switch (event.data.command) {
           // This command returns a list of the URLs corresponding to the Request objects
           // that serve as keys for the current cache.
           case "keys":
-            return cache
-              .keys()
-              .then(function(requests) {
-                var urls = requests.map(function(request) {
-                  return request.url;
-                });
-
-                return urls.sort();
-              })
-              .then(function(urls) {
-                // event.ports[0] corresponds to the MessagePort that was transferred as part of the controlled page's
-                // call to controller.postMessage(). Therefore, event.ports[0].postMessage() will trigger the onmessage
-                // handler from the controlled page.
-                // It's up to you how to structure the messages that you send back; this is just one example.
-                event.ports[0].postMessage({
-                  error: null,
-                  urls: urls,
-                });
+            cache.keys().then(function(requests) {
+              var urls = requests.map(function(request) {
+                return request.url;
               });
 
+              resolve(urls.sort());
+            });
+            break;
           // This command adds a new request/response pair to the cache.
           case "add":
             // If event.data.url isn't a valid URL, new Request() will throw a TypeError which will be handled
             // by the outer .catch().
 
             // do not cache localhost requests
-            var hostname = parseURL(event.data.url).hostname;
+            var hostname = new URL(event.data.url).hostname;
             if (
               !hostname ||
               hostname === "localhost" ||
               hostname === "127.0.0.1"
             ) {
               console.log("Skip caching local file " + event.data.url);
+              resolve();
               return;
             }
 
             var request = new Request(event.data.url);
-            return fetch(request)
+            fetch(request)
               .then(function(response) {
-                plugin_requirements.add(event.data.url);
-                // console.log("Caching requirement: " + event.data.url);
-                return cache.put(event.data.url, response);
+                cached_keys.add(event.data.url);
+                console.log("Caching requirement: " + event.data.url);
+                cache
+                  .put(event.data.url, response)
+                  .then(resolve)
+                  .catch(reject);
               })
               .catch(function(e) {
-                event.ports[0].postMessage({
-                  error: e,
-                });
+                console.error("Failed to cache requirement: " + event.data.url);
+                reject(e);
               });
-
+            break;
           // This command removes a request/response pair from the cache (assuming it exists).
           case "delete":
-            plugin_requirements.delete(event.data.url);
-            return cache.delete(event.data.url).then(function(success) {
-              event.ports[0].postMessage({
-                error: success ? null : "Item was not found in the cache.",
-              });
+            cached_keys.delete(event.data.url);
+            cache.delete(event.data.url).then(function(success) {
+              if (success) {
+                resolve();
+              } else {
+                reject("Item was not found in the cache.");
+              }
             });
-
+            break;
           default:
             // This will be handled by the outer .catch().
-            throw Error("Unknown command: " + event.data.command);
+            reject(new Error("Unknown command: " + event.data.command));
         }
       });
     }
