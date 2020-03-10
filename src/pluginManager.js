84065f4d4ada686a62bb5ed64d36f495f004b88a
Wei Ouyang <oeway007@gmail.com>
2020-03-10 20:54:42 +0100

    fix format

--- src/pluginManager.js
+++ src/pluginManager.js
@@ -309,18 +309,21 @@ export class PluginManager {
       config.asset_url = this.jailed_asset_url;
     }
 
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.ready
-      .then(() => {
-
-        navigator.serviceWorker.addEventListener('controllerchange', () => {
-          console.log('Service worker is active and the controller is available, caching internal plugins...');
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.ready.then(() => {
+        navigator.serviceWorker.addEventListener("controllerchange", () => {
+          console.log(
+            "Service worker is active and the controller is available, caching internal plugins..."
+          );
           for (let pn in this.internal_plugins) {
             this.normalizePluginUrl(this.internal_plugins[pn].uri).then(obj => {
               if (obj.uri) {
                 cacheUrlInServiceWorker(obj.uri)
                   .then(() => {
-                    console.log("Internal plugin cached in the service worker", obj.uri);
+                    console.log(
+                      "Internal plugin cached in the service worker",
+                      obj.uri
+                    );
                   })
                   .catch(e => {
                     console.error(e);
@@ -329,13 +332,10 @@ export class PluginManager {
             });
           }
         });
-        
       });
     } else {
-      console.log('Service workers are not supported.');
+      console.log("Service workers are not supported.");
     }
-
-    
 
     await initializeJailed(config);
 
