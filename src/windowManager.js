e686d118d7203249f0dbe82ef18857303b81bf67
Wei Ouyang <oeway007@gmail.com>
2020-08-15 00:17:26 +0200

    Wait 800ms for handling close event (#70)
    * Wait 800ms for handling close event
    
    * wait for closing
    
    * bump imjoy rpc version

--- src/windowManager.js
+++ src/windowManager.js
@@ -173,14 +173,15 @@ export class WindowManager {
       w.api.emit("hide");
     };
 
-    w.api.close = w.close = async () => {
-      try {
+    w.api.close = w.close = () => {
+      return new Promise(resolve => {
         w.api.emit("close");
-      } catch (es) {
-        console.error(es);
-      } finally {
-        this.closeWindow(w);
-      }
+        // leave 800ms for handling close events
+        setTimeout(() => {
+          this.closeWindow(w);
+          resolve();
+        }, 800);
+      });
     };
   }
 
