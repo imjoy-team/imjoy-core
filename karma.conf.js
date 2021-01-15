eca322e543c4db87cf2442a49d495e293ae831fb
Wei Ouyang <oeway007@gmail.com>
2021-01-15 14:21:38 +0100

    Remove firefox for now

--- karma.conf.js
+++ karma.conf.js
@@ -86,7 +86,7 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['ChromeHeadlessNoSandbox', 'FirefoxHeadless'],
+        browsers: ['ChromeHeadlessNoSandbox'],
 
         customLaunchers: {
             ChromeHeadlessNoSandbox: {
