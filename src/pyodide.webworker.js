058b088dfaf22640e52135a49ddf5574bd9af276
Wei Ouyang <oeway007@gmail.com>
2021-01-21 20:15:47 +0100

    Disable web-python tests for now

--- src/pyodide.webworker.js
+++ src/pyodide.webworker.js
@@ -1,6 +1,6 @@
 // webworker for running imjoy plugin with pyodide
-self.languagePluginUrl = "https://cdn.jsdelivr.net/pyodide/0.16.1/full/";
-importScripts("https://cdn.jsdelivr.net/pyodide/0.16.1/full/pyodide.js");
+self.languagePluginUrl = "https://cdn.jsdelivr.net/pyodide/v0.16.1/full/";
+importScripts("https://cdn.jsdelivr.net/pyodide/v0.16.1/full/pyodide.js");
 
 const src = `
 from imjoy import api
